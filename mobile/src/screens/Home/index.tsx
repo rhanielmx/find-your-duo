5de701a0f5951203ac3a5b7c40b06ab61a140920
rhanielmx <rhanielmag.xavier@gmail.com>
2022-09-13 00:59:55 -0300

    basic strutures with splash screen home screen and loading screen

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,38 @@
+import { FlatList, Image, View } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { GameCard } from '../../components/GameCard';
+import { Heading } from '../../components/Heading';
+import { styles } from './styles';
+
+import { GAMES } from '../../utils/games';
+
+export function Home() {
+    return (
+        <View style={styles.container}>
+            <Image
+                source={logoImg}
+                style={styles.logo}
+            />
+            <Heading
+                title="Encontre o seu duo."
+                subtitle="Seleione o game que deseja jogar..."
+            />
+
+            <FlatList
+                data={GAMES}
+                keyExtractor={item => item.id}
+                renderItem={({item}) => (
+                    < GameCard
+                        data={item}
+                    />
+                )}
+                showsHorizontalScrollIndicator={false}
+                horizontal
+                contentContainerStyle={styles.contentList}
+            />
+
+
+        </View>
+    );
+}
